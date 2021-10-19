4b6cb077eba1b8ba11d30b431dd99595aa134eed
vinricardo <vinicius.01deagosto@gmail.com>
2021-10-18 21:41:47 -0300

    feature: init node authentication

---
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+/**
+ * Receber code(string)
+ * Recuperar o access_token no github
+ * Verificar se o usuário existe no DB
+ * ---- SIM - Gera um token
+ * ---- NAO - Cria no DB, gera um token
+ * Retornar o token com as infos do user
+ */
+class AuthenticateUserService{
+    async execute(code: string) {
+        const url = "https://github.com/login/oauth/access_token";
+        const response= await axios.post(url,null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code
+            },
+            headers: {
+                "Accept": "application/json"
+            }
+        })
+        return response.data;
+    }
+}
+
+export { AuthenticateUserService }
