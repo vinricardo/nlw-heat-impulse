db18725f2a0d97d5e2919c727be696c9d0c5574d
vinricardo <vinicius.01deagosto@gmail.com>
2021-10-19 08:11:01 -0300

    feature: create user part one

--- node_heat/src/services/AuthenticateUserService.ts
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -1,27 +1,55 @@
-import axios from 'axios';
+import axios from "axios";
 /**
  * Receber code(string)
  * Recuperar o access_token no github
+ * Recuperar infos do user no github
  * Verificar se o usuário existe no DB
  * ---- SIM - Gera um token
  * ---- NAO - Cria no DB, gera um token
  * Retornar o token com as infos do user
  */
-class AuthenticateUserService{
-    async execute(code: string) {
-        const url = "https://github.com/login/oauth/access_token";
-        const response= await axios.post(url,null, {
-            params: {
-                client_id: process.env.GITHUB_CLIENT_ID,
-                client_secret: process.env.GITHUB_CLIENT_SECRET,
-                code
-            },
-            headers: {
-                "Accept": "application/json"
-            }
-        })
-        return response.data;
-    }
+
+interface IAcessTokenResponse {
+  access_token: string;
 }
 
-export { AuthenticateUserService }
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+    const { data: acessTokenResponse } = await axios.post<IAcessTokenResponse>(
+      url,
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    const response = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${acessTokenResponse.access_token}`,
+        },
+      }
+    );
+
+    const { login, id, avatar_url, name } = response.data;
+
+    return response.data;
+  }
+}
+
+export { AuthenticateUserService };
